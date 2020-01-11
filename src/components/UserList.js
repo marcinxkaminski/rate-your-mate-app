003120377a926222424b8bc0e732cdff70955488
piotrkedra <eloelo153>
2020-01-11 20:50:51 +0100

    basic list

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image  } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 export default class UserList extends React.Component{
@@ -23,16 +24,16 @@ export default class UserList extends React.Component{
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         });
-        var path = 'https://rate-your-mate.herokuapp.com/api/v1/users';
+        var path = 'http://rate-your-mate.herokuapp.com/api/v1/users';
         fetch(path, {
             method: 'GET',
             headers: header,
         })
-        .then((response) => response.json())
+        .then((response) => {return response.json()})
         .then((responseJSON) => {
             
             console.log(responseJSON); 
-            this.setState({users: responseJSON})
+            this.setState({users: Object.values(responseJSON)})
 
         })
         .catch((error) =>{
@@ -40,23 +41,87 @@ export default class UserList extends React.Component{
         });
     }
 
-    //https://rate-your-mate.herokuapp.com/api/v1/users
-
     render(){
         return(
-            <TouchableOpacity >
+            <View >
                 <FlatList 
+                    style={styles.listContainer}
                     data={this.state.users}
                     renderItem={({item}) => (
                         <TouchableOpacity
+                            style={styles.userLabelContainer}
                             onPress={() => this.state.nav.navigate('PropertiesPanel', {user: item})}
-                        >
-                            <Text>{item.name}</Text>
+                            >  
+                            <View style={styles.imageContainer}>
+                                <Image
+                                    style={{width: 60, height: 60, borderRadius: 30}}
+                                    source={{uri: item.avatar}}
+                                />
+                             </View>
+                            <View style={styles.infoContainer}>
+                                <Text style={styles.nameText}>{item.name}</Text>
+                                <Text style={styles.positionText}>{item.position}</Text>
+                            </View>
+                            <View style={styles.starsContainer}>
+                                <Text style={styles.starText}>{item.stars}</Text> 
+                                <Icon name="star-outline" size={35} color="#430098"/>
+                            </View>
                         </TouchableOpacity>
                     )}
                     keyExtractor={item => JSON.stringify(item.id)}
                 />
-            </TouchableOpacity>
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    listContainer: {
+        width: '90%',
+        flex: 1, 
+        flexDirection: 'column',
+        borderWidth: 1
+    },
+    userLabelContainer: {
+        flex: 1, 
+        flexDirection: 'row',
+        borderWidth: 1
+    },
+    nameText: {
+        fontSize: 15
+    },
+    positionText: {
+        fontSize: 13
+    },
+    customContainer: {
+        padding: 5,
+        paddingLeft: 10,
+        paddingRight: 15,
+        borderWidth: 1,
+        justifyContent: 'flex-start'
+    },
+    imageContainer: {
+        width: '25%',
+        padding: 5,
+        borderWidth: 1,
+        alignItems: 'center'
+    },
+    infoContainer: {
+        width: '50%',
+        padding: 5,
+        paddingLeft: 10,
+        paddingRight: 15,
+        justifyContent: 'flex-start'
+    },
+    starsContainer: {
+        width: '25%',
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+    starText: {
+        fontSize: 20
+    }
+  });
+  
