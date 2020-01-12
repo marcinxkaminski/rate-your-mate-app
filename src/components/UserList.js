b34611303cdf1d105a12ebfad6e5d93fa513e53b
piotrkedra <eloelo153>
2020-01-12 05:06:34 +0100

    modal

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image  } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Modal from "react-native-modal";
+import SectionedMultiSelect from 'react-native-sectioned-multi-select';
+
+
 
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
@@ -12,11 +16,31 @@ export default class UserList extends React.Component{
     state = {
         users: [],
         nav: this.props.nav,
-        dataLoaded: false
+        dataLoaded: false,
+        searchModal: false,
+        categories: [
+            {
+                name: 'Categories',
+                id: 0, 
+                children: [
+                    {
+                        name: 'java',
+                        id: 15,
+                      },
+                      {
+                        name: 'javascript',
+                        id: 16,
+                      },
+                ] 
+            }
+            ],
+        selectedItems: [],
+        filtredUsers: []
     }
 
     componentDidMount = () => {
         this.getUsers();
+        this.getCategories();
     }
 
     getUsers = () => {
@@ -31,10 +55,34 @@ export default class UserList extends React.Component{
         })
         .then((response) => {return response.json()})
         .then((responseJSON) => {
-            
-            console.log(responseJSON); 
-            this.setState({users: Object.values(responseJSON)})
+            this.setState({users: Object.values(responseJSON), filtredUsers: Object.values(responseJSON)})
+        })
+        .catch((error) =>{
+            console.error(error);
+        });
+    }
 
+    getCategories = () => {
+        var header = new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        });
+        var path = 'http://rate-your-mate.herokuapp.com/api/v1/categories';
+        fetch(path, {
+            method: 'GET',
+            headers: header,
+        })
+        .then((response) => {return response.json()})
+        .then((responseJSON) => {
+            var obj = [
+                {
+                    name: 'Categories',
+                    id: 0, 
+                    children: responseJSON
+                }   
+            ]
+
+            this.setState({categories: obj})
         })
         .catch((error) =>{
             console.error(error);
@@ -46,6 +94,66 @@ export default class UserList extends React.Component{
           'MerriweatherSans': require('../../assets/fonts/Questrial-Regular.ttf'),
         });
     };
+
+    onSelectedItemsChange = (selectedItems) => {
+        this.setState({ selectedItems });
+      };
+
+    firstSearch = () => {
+        //this.resetFitredUsers();
+        this.setState({searchModal: true})
+    }
+
+    resetFitredUsers = () => {
+        this.setState({filtredUsers: this.state.users});
+    }
+
+    filterUsers = () => {
+        console.log(this.state.selectedItems.length)
+        if(this.state.selectedItems.length == 0){
+            this.setState({searchModal: false, filtredUsers: this.state.users});
+            return;
+        }
+        var users = this.state.users;
+        var currentCategories = this.getFilterdCategories();
+        var result = [];
+        users.forEach(user => {
+            var categories = this.prepareCategoris(Object.values(user.categories))
+            if(this.ifCommonElement(categories, currentCategories) == true){
+                result.push(user)
+            }
+        });
+        this.setState({searchModal: false, filtredUsers: result});
+    }
+
+    getFilterdCategories = () => {
+        var result = []
+        this.state.categories[0].children.forEach(ele => {
+            if(this.state.selectedItems.includes(ele.id)){
+                result.push(ele.name);
+            }
+        })
+        return result;
+    }
+
+    prepareCategoris = (cat) => {
+        var result = [];
+        cat.forEach(category => {
+            result.push(category.name);
+        })
+        return result;
+    }
+
+    ifCommonElement(array1, array2) { 
+        for(let i = 0; i < array1.length; i++) { 
+            for(let j = 0; j < array2.length; j++) { 
+                if(array1[i] === array2[j]) { 
+                    return true; 
+                } 
+            } 
+        } 
+        return false;  
+    } 
 
     render = () => {
         if(this.state.dataLoaded == false){
@@ -60,7 +168,7 @@ export default class UserList extends React.Component{
             <View >
                 <FlatList 
                     style={styles.listContainer}
-                    data={this.state.users}
+                    data={this.state.filtredUsers}
                     renderItem={({item}) => (
                         <TouchableOpacity
                             style={styles.userLabelContainer}
@@ -84,6 +192,40 @@ export default class UserList extends React.Component{
                     )}
                     keyExtractor={item => JSON.stringify(item.id)}
                 />
+                <TouchableOpacity style={styles.searchBox} onPress={this.firstSearch}>
+                    <Icon name="cloud-search"  size={35} color="white" />
+                </TouchableOpacity>
+                <Modal 
+                    isVisible={this.state.searchModal}
+                    style={styles.modal}
+                    backdropColor={'rgba(52, 52, 52, 0.5)'}
+                    backdropOpacity={1}
+                    animationIn={'bounceInDown'}
+                    animationOut={'bounceOutUp'}
+                    animationInTiming={500}
+                    animationOutTiming={500}
+                    backdropTransitionInTiming={500}
+                    backdropTransitionOutTiming={500}>
+                    <View style={{ flex: 1, borderWidth:1, height: '100%', backgroundColor: '#FBFBFD', padding: 10}}>
+                        <View style={{height: '80%'}}>
+                        <SectionedMultiSelect
+                            items={this.state.categories}
+                            uniqueKey="id"
+                            subKey="children"
+                            selectText="Choose some things..."
+                            showDropDowns={false}
+                            readOnlyHeadings={true}
+                            onSelectedItemsChange={this.onSelectedItemsChange}
+                            selectedItems={this.state.selectedItems}
+                        />
+                        </View>
+                        <View style={{position: 'absolute', bottom: 5, right: '35%'}}>
+                            <TouchableOpacity onPress={this.filterUsers} style={styles.modalSearchBox}>
+                                <Icon name="cloud-search"  size={35} color="white" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
             </View>
         )
     }
@@ -130,6 +272,37 @@ const styles = StyleSheet.create({
     },
     starText: {
         fontSize: 20
+    },
+    searchBox: {
+        borderWidth:1,
+        borderColor:'rgba(0,0,0,0.2)',
+        alignItems:'center',
+        justifyContent:'center',
+        width:80,
+        position: 'absolute',                                          
+        bottom: 10,                                                    
+        right: 20,
+        height:80,
+        backgroundColor:'#79589F',
+        borderRadius:100,
+    },
+    modalSearchBox: {
+        borderWidth:1,
+        borderColor:'rgba(0,0,0,0.2)',
+        alignItems:'center',
+        justifyContent:'center',
+        width:80,
+        height:80,
+        backgroundColor:'#79589F',
+        borderRadius:100,
+    },
+    modal: {
+        backgroundColor: 'rgba(52, 52, 52, 0)',
+        marginHorizontal: '10%',
+        marginTop: '40%',
+        borderRadius: 5,
+        alignItems: undefined,
+        justifyContent: undefined,
     }
   });
   
--- src/main/MainContainer.js
+++ src/main/MainContainer.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import UserList from '../components/UserList'
-import { TextInput } from 'react-native-gesture-handler';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
 
 
 export default class MainContainer extends React.Component{
@@ -19,14 +22,6 @@ export default class MainContainer extends React.Component{
     render = () => {
         return(
             <View style={styles.container}>
-                <View style={styles.searchContainer}>
-                    <TextInput
-                        style={styles.searchBox}
-                        onChangeText={text => this.setState({searchText: text})}
-                        value={this.state.searchText}
-                        placeholder="Search something"
-                    />
-                </View>
                 <UserList nav={this.state.nav}/>
             </View>
         )
@@ -35,28 +30,9 @@ export default class MainContainer extends React.Component{
 
 const styles = StyleSheet.create({
     container: {
-      alignItems: 'center',
-      backgroundColor: '#fff',
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-    },
-    searchContainer: {
-        backgroundColor: '#79589F',
-        justifyContent: 'flex-end',
+        marginTop: '7%',
         alignItems: 'center',
-        paddingBottom: 20,
-        marginTop: 100,
-        height: '30%',
-        width: '100%',
-    },
-    searchBox: {
-        backgroundColor: 'white',
-        width: '90%',
-        padding: 5,
-        paddingLeft: 10,
-        borderRadius: 10,
-        marginTop: 5,
-        fontSize: 15
+        backgroundColor: '#fff',
+        flex: 1,
     },
   });
